a8deca738d8c45692b7b8e4ecde955bb22012edc
Sam Dillard <sam@influxdata.com>
2023-07-04 11:51:56 -0700

    Start of first task page. Date picker overlay works.

--- Votion/views.swift
+++ Votion/views.swift
@@ -70,3 +70,95 @@ struct AppBar: View {
         .background(Color.black)
     }
 }
+
+struct PersonalTaskForm: View {
+    @State private var name: String = ""
+    @State private var startDate: Date = Date()
+    @State private var isDatePickerShown: Bool = false
+    @State private var description: String = ""
+        
+    @State private var selectedDateString: String = ""
+        
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            HStack {
+                Spacer()
+                VStack {
+                    Image(systemName: "person")
+                    Text("Create personal task")
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding(.bottom, 100)
+                .font(.largeTitle)
+                Spacer()
+            }
+            Text("Task name")
+                .font(.headline)
+            
+            TextField("Enter task name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Start Date")
+                .font(.headline)
+            
+            TextField("Select start date", text: $selectedDateString, onEditingChanged: { isEditing in
+                if isEditing {
+                    isDatePickerShown = true
+                }
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .onTapGesture {
+                isDatePickerShown = true
+            }
+            .padding(.bottom, 8)
+            
+            Text("Description")
+                .font(.headline)
+            
+            TextEditor(text: $description)
+                .frame(height: 100)
+                .border(Color.gray, width: 1)
+        }
+        .padding()
+        .overlay(
+            Group {
+                if isDatePickerShown {
+                    Color.black.opacity(0.3)
+                        .edgesIgnoringSafeArea(.all)
+                        .onTapGesture {
+                            isDatePickerShown = false
+                        }
+                    
+                    VStack {
+                        Spacer()
+                        
+                        DatePicker("", selection: Binding(
+                            get: {
+                                let formatter = DateFormatter()
+                                formatter.dateStyle = .medium
+                                return formatter.date(from: selectedDateString) ?? Date()
+                            },
+                            set: { date in
+                                let formatter = DateFormatter()
+                                formatter.dateStyle = .medium
+                                selectedDateString = formatter.string(from: date)
+                                startDate = date
+                            }
+                        ), displayedComponents: .date)
+                        .datePickerStyle(WheelDatePickerStyle())
+                        .labelsHidden()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .padding()
+                    }
+                }
+            }
+        )
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalTaskForm()
+    }
+}
+
